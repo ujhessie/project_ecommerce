9dfc3c4f0514c09f51e3d58542ac83417c17afae
Jesse Rodrigues <98673779+ujhessie@users.noreply.github.com>
2023-10-06 20:11:56 -0300

    Teste de url

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base:"/project_ecommerce",
+  base:"./project_ecommerce",
   plugins: [react()],
 })
